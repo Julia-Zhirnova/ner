c6f02b9f853def232cf5f9888bb8e44cf4b3e211
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2022-05-05 19:32:43 +0300

    Update ner.py

--- ner.py
+++ ner.py
@@ -6,6 +6,19 @@ import numpy as np
 import os
 from datetime import datetime
 
+import tensorflow as tf
+from utils import backbone
+from NEURO.api import object_counting_api
+
+detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
+
+is_color_recognition_enabled = False # set it to true for enabling the color prediction for the detected objects
+roi = 385 # roi line position
+deviation = 2 # the constant that represents the object counting area
+custom_object_name = 'Pedestrian'
+
+
+
 # User quit method message 
 print("You can press 'Q' to quit this script.")
 
@@ -42,8 +55,11 @@ avgtime = 0
 # Capture frames from the camera
 for frame in camera.capture_continuous(capture, format="bgra", use_video_port=True, resize=(img_width,img_height)):
     counter+=1
-    cv2.imshow("pair", frame)
+    cv2.imshow("pair", frame)    
     key = cv2.waitKey(1) & 0xFF
+    object_counting_api.cumulative_object_counting_x_axis(frame, detection_graph, category_index,
+                                                          is_color_recognition_enabled, roi, deviation,
+                                                          custom_object_name)  # counting all the objects
     # if the `q` key was pressed, break from the loop and save last image
     if key == ord("q") :
         t1 = datetime.now()
